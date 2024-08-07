72750babf29f6e10ab9064f73f13f7aa9f5469b1
Michael Steenkamp <michael.steenkamp@icloud.com>
2024-08-07 19:47:53 +0800

    V0.6
    Main Menu v0.1

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        Debug.Log("Play Game");
+    }
+
+    public void Settings()
+    {
+        Debug.Log("Settings");
+    }
+    public void Credits()
+    {
+        Debug.Log("Credits");
+    }
+    public void Exit()
+    {
+        Debug.Log("Exit");
+    }
+
+}
